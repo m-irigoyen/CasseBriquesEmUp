f721d7475f97a9c94bf0db0becccd5b9b1d629a6
Xyunn <maxime.devecchi@gmail.com>
2015-10-15 22:57:12 +0200

    Init repo

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMode : MonoBehaviour {
+
+	private int m_score = 0;
+	private int m_playerHealth = 100;
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void updateHud()
+	{
+
+	}
+
+	public void setHealthPlayer()
+	{
+	
+	}
+
+	void switchPause()
+	{
+
+	}
+
+	public void updateScore(int p_score)
+	{
+
+	}
+}
---
+++ Assets/Scripts/Global/Tags.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+	public const string m_mainCamera = "MainCamera";
+	public const string m_player = "Player";
+	public const string m_gameMode = "GameMode";
+	public const string m_ball = "Ball";
+}
