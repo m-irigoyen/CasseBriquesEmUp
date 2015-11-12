5e5035bfb7006fa12bbcdd0e6e7889907447cc4e
Milan "Edrevan" irigoyen <edrevan@gmail.com>
2015-11-12 17:29:09 +0100

    Created paddleController script

---
+++ Assets/Scripts/Global/PaddleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour
+{
+    public Transform m_paddleAnchor;
+    public float m_maxDistance;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
